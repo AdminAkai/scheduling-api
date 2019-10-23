1e818a1e5c9a43fc745d586ec42904711dacca28
JCaptain <joshietrinidad@gmail.com>
2019-10-23 15:25:46 -0400

    All main functions (CRUD for users, jobs, and schedule) functioning

--- controllers/userscontroller.js
+++ controllers/userscontroller.js
@@ -30,6 +30,12 @@ userTrackerRouter.put('/users/edit/:id', (req,res) => {
   })
 })
 
+userTrackerRouter.delete('/users/delete/:id', (req,res) => {
+  userTrackerApi.deleteUser(req.params.id).then((deleteduser) => {
+    res.redirect('/')  
+  })
+})
+
 // user authentication
 userTrackerRouter.post('/dashboard', (req, res) => {
   userTrackerApi.verifyAuth(req.body.username, req.body.password).then((currentUser) => {
@@ -90,9 +96,9 @@ userTrackerRouter.delete('/schedule/delete/:id', (req,res) => {
 })
 
 // display jobs  
-userTrackerRouter.get('/dashboard/view-jobs', (req,res) => {
+userTrackerRouter.get('/dashboard/view-jobs/:id', (req,res) => {
   userTrackerApi.getAllJobs().then((allJobs) => {
-    userTrackerApi.getUserSchedules(req.params.id).then((currentDashboard) => {
+    userTrackerApi.getUser(req.params.id).then((currentUser) => {
         res.render('alljobs', {allJobs, currentUser})
     })
   })
@@ -117,115 +123,6 @@ userTrackerRouter.post('/job/create', (req,res) => {
     })
   })
 })
-
-// userTrackerRouter.post('/dashboard', (req, res) => {
-//   console.log(req.body)
-//   userTrackerApi.verifyAuth(req.body.username, req.body.password).then((currentUser) => {
-//     if (req.body.username === 'admin') {
-//       res.redirect(`/dashboard/admin/${currentUser._id}`)
-//     } else {
-//       res.redirect(`/dashboard/${currentUser._id}`)
-//     }
-//   })
-// })
-
-
-// userTrackerRouter.post('/schedule/create', (req,res) => {
-//   userTrackerApi.addNewSchedule(req.body).then((newSchedule) => {
-//     res.render('schedule', newSchedule)
-//   })
-// })
-
-// userTrackerRouter.get('/users/edit/:id', (req, res) => {
-//   userTrackerApi.getUser(req.params.id).then((user) => {
-//     res.render('edituser', user)
-//   })
-// })
-
-// userTrackerRouter.put('/users/edit/:id', (req,res) => {
-//   userTrackerApi.updateUser(req.params.id, req.body).then((updateduser) => {
-//     res.redirect(`/users/user/${req.params.id}`)
-//   })
-// })
-
-// userTrackerRouter.get('/schedule/:id', (req, res) => {
-//   userTrackerApi.getSchedule(req.params.id).then((schedule) => {
-//     userTrackerApi.getAllUsers().then((allUsers) => {
-//       res.render('editschedule', {schedule, allUsers})
-//     })
-//   })
-// })
-
-// userTrackerRouter.put('/schedule/edit/:id', (req,res) => {
-//   userTrackerApi.updateSchedule(req.params.id, req.body).then((updatedSchedule) => {
-//     userTrackerApi.getSpecificUser('admin').then((admin) => {
-//       res.redirect(`/dashboard/admin/${admin._id}`)
-//     })
-//   })
-// })
-
-// userTrackerRouter.delete('/users/delete/:id', (req,res) => {
-//   userTrackerApi.deleteUser(req.params.id).then((deleteduser) => {
-//     res.redirect('/users')  
-//   })
-// })
-
-// userTrackerRouter.delete('/schedule/delete/:id', (req,res) => {
-//   userTrackerApi.deleteSchedule(req.params.id).then((deletedschedule) => {
-//     userTrackerApi.getAllSchedules().then((allSchedules) => {
-//       userTrackerApi.getSpecificUser('admin').then((currentUser) => {
-//         res.render('allSchedules', {allSchedules, currentUser})
-//       })
-//     })        
-//   })
-// })
-// userTrackerRouter.get('/', (req, res) => {
-//   res.render('users')
-// })
-
-// userTrackerRouter.get('/login', (req, res) => {
-//     res.render('login')
-// })
-
-// userTrackerRouter.get('/users', (req, res) => {
-//   userTrackerApi.getAllUsers().then((users) => {
-//     res.render('users', {users})
-//   })
-// })
-
-// userTrackerRouter.get('/users/user/:id', (req, res) => {
-//   userTrackerApi.getUser(req.params.id).then((user) => {
-//     res.render('user', user)
-//   })
-// })
-
-// userTrackerRouter.get('/users/new', (req, res) => {
-//   res.render('createuser')
-// })
-
-// userTrackerRouter.get('/users/edit/:id', (req, res) => {
-//   userTrackerApi.getUser(req.params.id).then((user) => {
-//     res.render('edituser', user)
-//   })
-// })
-
-// userTrackerRouter.post('/users/create', (req,res) => {
-//   userTrackerApi.addNewUser(req.body).then((newuser) => {
-//     res.redirect(`/users/user/${newuser._id}`)
-//   })
-// })
-
-// userTrackerRouter.put('/users/edit/:id', (req,res) => {
-//   userTrackerApi.updateUser(req.params.id, req.body).then((updateduser) => {
-//     res.redirect(`/users/user/${req.params.id}`)
-//   })
-// })
-
-// userTrackerRouter.delete('/users/delete/:id', (req,res) =>{
-//   userTrackerApi.deleteUser(req.params.id).then((deleteduser) => {
-//     res.redirect('/users')  
-//   })
-// })
 
 module.exports = {
   userTrackerRouter
